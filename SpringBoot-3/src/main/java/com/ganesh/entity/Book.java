71bb95bee67642bbfc6d03c00c15d705d41f4072
Ganesh <ganeshgarje716@gmail.com>
2026-05-29 17:48:31 +0530

    Add @NoArgsConstructor Annotation

--- SpringBoot-3/src/main/java/com/ganesh/entity/Book.java
+++ SpringBoot-3/src/main/java/com/ganesh/entity/Book.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "Books")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Book {
 	
