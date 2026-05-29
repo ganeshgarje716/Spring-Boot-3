368549e84ac40a8e1cd0e283afae09acfa1bbd0e
Ganesh <ganeshgarje716@gmail.com>
2026-05-29 18:22:10 +0530

    Add @toString Annotation

--- SpringBoot-3/src/main/java/com/ganesh/entity/Book.java
+++ SpringBoot-3/src/main/java/com/ganesh/entity/Book.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "Books")
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Book {
 	
 	
