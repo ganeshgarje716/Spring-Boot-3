21c5af184eb00c747bb2b934e3342b14bbe31466
Ganesh <ganeshgarje716@gmail.com>
2026-05-29 17:23:19 +0530

    Create Variables in Book Class

--- SpringBoot-3/src/main/java/com/ganesh/entity/Book.java
+++ SpringBoot-3/src/main/java/com/ganesh/entity/Book.java
@@ -1,8 +1,30 @@
 package com.ganesh.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "Books")
+@Getter
+@Setter
+@AllArgsConstructor
 public class Book {
+	
+	
+	@Id
+	private Integer id;
+	
+	@Column(name = "Book_Name", unique = true)
+	private String name;
+	
+	private Double price;
+	
+	private Integer qty;
+	
 
 }
