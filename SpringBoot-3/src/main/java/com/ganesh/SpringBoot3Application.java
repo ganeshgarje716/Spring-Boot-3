5f44d2a4afc41e9cd06987d24128e669e0996c95
Ganesh <ganeshgarje716@gmail.com>
2026-05-29 17:40:12 +0530

    implement CommandLineRunner Interface

--- SpringBoot-3/src/main/java/com/ganesh/SpringBoot3Application.java
+++ SpringBoot-3/src/main/java/com/ganesh/SpringBoot3Application.java
@@ -1,13 +1,27 @@
 package com.ganesh;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ganesh.Repository.BookRepository;
+
 @SpringBootApplication
-public class SpringBoot3Application {
+public class SpringBoot3Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot3Application.class, args);
+	}
+	
+	
+	@Autowired
+	BookRepository bookRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
 	}
 
 }
