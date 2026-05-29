18e87842a5bb8c86995cd831a9a1d875a594514e
Ganesh <ganeshgarje716@gmail.com>
2026-05-29 17:40:44 +0530

    Implement Book Class

--- SpringBoot-3/src/main/java/com/ganesh/Repository/BookRepository.java
+++ SpringBoot-3/src/main/java/com/ganesh/Repository/BookRepository.java
@@ -1,9 +1,10 @@
 package com.ganesh.Repository;
 
-import java.awt.print.Book;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.ganesh.entity.Book;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer>{
